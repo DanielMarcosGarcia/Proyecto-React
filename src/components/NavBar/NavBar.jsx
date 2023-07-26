77b91f7713e8e370e6e0cd173d010b445369c858
Daniel Garcia <danielgarciamarco17@gmail.com>
2023-07-26 20:35:14 -0300

    NavBar Agregado

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>Tienda de Cosmeticos</h3>
+            <div>
+                <button>Esmaltes</button>
+                <button>Accesorios</button>
+                <button>Cremas</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
